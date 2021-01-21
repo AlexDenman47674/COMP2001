f4b741840533cafc5b9d0d983d2db8e8a9827414
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2021-01-21 15:12:44 +0000

    Added stored procedure for verification of users

--- COMP2001Part1.2/COMP2001Part1.2/Models/COMP2001_ADenmanContext.cs
+++ COMP2001Part1.2/COMP2001Part1.2/Models/COMP2001_ADenmanContext.cs
@@ -24,11 +24,11 @@ namespace COMP2001Part1._2.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=socem1.uopnet.plymouth.ac.uk;Database=COMP2001_ADenman;User Id=ADenman;Password=AjpA974+");
-            }
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=socem1.uopnet.plymouth.ac.uk;Database=COMP2001_ADenman;User Id=ADenman;Password=AjpA974+");
+//            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ COMP2001Part1.2/COMP2001Part1.2/Models/ValidateUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2001Part1._2.Models
+{
+    public class ValidateUser
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
